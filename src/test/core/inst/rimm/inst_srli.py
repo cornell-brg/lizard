1bd83762dc123990400c33be4c012b53d589d4e9
Jacob Glueck <swimgiraffe435@gmail.com>
2018-10-22 12:33:36 -0400

    fix srli tests for bitwidth

--- src/test/core/inst/rimm/inst_srli.py
+++ src/test/core/inst/rimm/inst_srli.py
@@ -57,7 +57,7 @@ def gen_basic_test():
 def gen_dest_dep_test():
   return [
       gen_rimm_dest_dep_test( i, "srli", 3 - i, i,
-                              Bits( 32, ( 3 - i ) ).uint() >> i )
+                              Bits( XLEN, ( 3 - i ) ).uint() >> i )
       for i in range( 0, 6 )
   ]
 
@@ -82,7 +82,7 @@ def gen_src_dep_test():
 def gen_srcs_dest_test():
   return [
       gen_rimm_src_eq_dest_test( "srli", 3 - i, i,
-                                 Bits( 32, ( 3 - i ) ).uint() >> i )
+                                 Bits( XLEN, ( 3 - i ) ).uint() >> i )
       for i in range( 0, 6 )
   ]
 
@@ -111,7 +111,7 @@ def gen_random_test():
   for i in xrange( 100 ):
     src = Bits( XLEN, random.randint( 0, 0xffffffff ) )
     imm = Bits( 5, random.randint( 0, 0x1f ) )
-    dest = Bits( 32, src.uint() >> imm.uint(), trunc=True )
+    dest = Bits( XLEN, src.uint() >> imm.uint(), trunc=True )
     asm_code.append(
         gen_rimm_value_test( "srli", src.uint(), imm.uint(), dest.uint() ) )
   return asm_code
