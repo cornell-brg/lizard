2e5126de0841149ae977581f5f8e8e58633d1537
Jacob Glueck <swimgiraffe435@gmail.com>
2018-11-12 15:27:42 -0500

    test all 3  opt levels

--- src/test/core/program_test.py
+++ src/test/core/program_test.py
@@ -15,15 +15,16 @@ TEST_DIR = "program/"
 
 procs = [ ProcTestHarnessFL, ProcTestHarnessCL ]
 proc_dict = dict([( x.__name__, x ) for x in procs ] )
-
+opt_levels = range( 4 )
 dir, tests = collector.collect()
 
 
+@pytest.mark.parametrize( 'opt_level', opt_levels )
 @pytest.mark.parametrize( 'program', tests )
 @pytest.mark.parametrize( 'proc_key', proc_dict.keys() )
-def test( proc_key, program ):
+def test( proc_key, program, opt_level ):
   # Build the program
   print( "Building: %s" % program )
-  outname = collector.build( program )
+  outname = collector.build( program, opt_level )
   # Run it
   run_test_elf( proc_dict[ proc_key ], outname, 10000 )
