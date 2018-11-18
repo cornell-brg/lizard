e0dcd1fbd274e325cade804b338ae7f3e5ffaaed
Jacob Glueck <swimgiraffe435@gmail.com>
2018-11-17 20:30:40 -0500

    make freelist bypassing

--- src/test/extra/freelist_test.py
+++ src/test/extra/freelist_test.py
@@ -2,13 +2,13 @@ from pymtl import *
 from util.test_utils import run_test_vector_sim
 from util.rtl.freelist import FreeList
 
-fucked = True
+pymtl_broken = True
 
 
 def test_basic():
   run_test_vector_sim(
-      FreeList( 4, 1, 1, 0 ), [
-          ( 'alloc_ports[0].call alloc_ports[0].ret.valid* alloc_ports[0].ret.index* free_ports[0].call free_ports[0].arg'
+      FreeList( 4, 1, 1, False ), [
+          ( 'alloc_ports[0].call alloc_ports[0].ret.valid* alloc_ports[0].ret.index* free_ports[0].call free_ports[0].arg.index'
           ),
           ( 1, 1, 0, 0, 0 ),
           ( 1, 1, 1, 0, 0 ),
@@ -20,12 +20,12 @@ def test_basic():
           ( 1, 1, 1, 0, 0 ),
       ],
       dump_vcd=None,
-      test_verilog=not fucked )
+      test_verilog=not pymtl_broken )
 
 
 def test_used_initial():
   run_test_vector_sim(
-      FreeList( 4, 1, 1, 2 ), [
+      FreeList( 4, 1, 1, False, 2 ), [
           ( 'alloc_ports[0].call alloc_ports[0].ret.valid* alloc_ports[0].ret.index* free_ports[0].call free_ports[0].arg'
           ),
           ( 1, 1, 2, 0, 0 ),
@@ -35,12 +35,12 @@ def test_used_initial():
           ( 1, 0, '?', 0, 0 ),
       ],
       dump_vcd=None,
-      test_verilog=not fucked )
+      test_verilog=not pymtl_broken )
 
 
 def test_reverse_free_order():
   run_test_vector_sim(
-      FreeList( 2, 1, 1, 0 ), [
+      FreeList( 2, 1, 1, False ), [
           ( 'alloc_ports[0].call alloc_ports[0].ret.valid* alloc_ports[0].ret.index* free_ports[0].call free_ports[0].arg'
           ),
           ( 1, 1, 0, 0, 0 ),
@@ -49,4 +49,18 @@ def test_reverse_free_order():
           ( 1, 1, 1, 0, 0 ),
       ],
       dump_vcd=None,
-      test_verilog=not fucked )
+      test_verilog=not pymtl_broken )
+
+
+def test_bypass():
+  run_test_vector_sim(
+      FreeList( 2, 1, 1, True ), [
+          ( 'alloc_ports[0].call alloc_ports[0].ret.valid* alloc_ports[0].ret.index* free_ports[0].call free_ports[0].arg'
+          ),
+          ( 1, 1, 0, 0, 0 ),
+          ( 1, 1, 1, 0, 0 ),
+          ( 1, 1, 1, 1, 1 ),
+          ( 1, 1, 0, 1, 0 ),
+      ],
+      dump_vcd=None,
+      test_verilog=not pymtl_broken )
--- src/util/rtl/freelist.py
+++ src/util/rtl/freelist.py
@@ -24,6 +24,7 @@ class FreeList( Model ):
                 nslots,
                 num_alloc_ports,
                 num_free_ports,
+                combinational_bypass,
                 used_slots_initial=0 ):
 
     nbits = clog2( nslots )
@@ -42,7 +43,8 @@ class FreeList( Model ):
     ]
 
     s.free = RegisterFile( nbits, nslots, num_alloc_ports, num_free_ports,
-                           False, [ x for x in range( nslots ) ] )
+                           combinational_bypass,
+                           [ x for x in range( nslots ) ] )
     s.size = Wire( ncount_bits )
     s.head = Wire( nbits )
     s.tail = Wire( nbits )
@@ -51,6 +53,7 @@ class FreeList( Model ):
         Wire( ncount_bits ) for _ in range( num_alloc_ports )
     ]
     s.free_size_next = [ Wire( ncount_bits ) for _ in range( num_free_ports ) ]
+    s.bypassed_size = Wire( ncount_bits )
     s.head_next = [ Wire( nbits ) for _ in range( num_alloc_ports ) ]
     s.tail_next = [ Wire( nbits ) for _ in range( num_free_ports ) ]
     s.head_incs = [
@@ -78,7 +81,7 @@ class FreeList( Model ):
         if s.free_ports[ port ].call:
           s.free.wr_en[ port ].v = 1
           s.free.wr_addr[ port ].v = ctail
-          s.free.wr_data = s.free_ports[ port ].arg.index
+          s.free.wr_data[ port ].v = s.free_ports[ port ].arg.index
 
           s.tail_next[ port ].v = s.tail_incs[ port ].out
           s.free_size_next[ port ].v = base - 1
@@ -86,6 +89,17 @@ class FreeList( Model ):
           s.free.wr_en[ port ].v = 0
           s.tail_next[ port ].v = ctail
           s.free_size_next[ port ].v = base
+
+    if combinational_bypass:
+
+      @s.combinational
+      def handle_bypass():
+        s.bypassed_size.v = s.free_size_next[ num_free_ports - 1 ]
+    else:
+
+      @s.combinational
+      def handle_bypass():
+        s.bypassed_size.v = s.size
 
     for port in range( num_alloc_ports ):
       if port == 0:
@@ -104,7 +118,7 @@ class FreeList( Model ):
           base = s.alloc_size_next[ port - 1 ]
           chead = s.head_next[ port - 1 ]
         s.free.rd_addr[ port ].v = chead
-        if s.alloc_ports[ port ].call and s.size != nslots:
+        if s.alloc_ports[ port ].call and s.bypassed_size != nslots:
           s.alloc_ports[ port ].ret.valid.v = 1
           s.head_next[ port ].v = s.head_incs[ port ].out
           s.alloc_size_next[ port ].v = base + 1
@@ -125,4 +139,5 @@ class FreeList( Model ):
         s.size.n = s.alloc_size_next[ num_alloc_ports - 1 ]
 
   def line_trace( s ):
-    return "hd:{}tl:{}:sz:{}".format( s.head.v, s.tail.v, s.size.v )
+    return "hd:{}tl:{}:sz:{}:ls:{}".format( s.head.v, s.tail.v, s.size.v,
+                                            s.free.line_trace() )
