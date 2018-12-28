a82583134ec78c9f94ac262a611538f4304d1aec
yz624 <yz624@cornell.edu>
2018-12-27 20:50:43 -0500

    Added method test for ring buffer

--- src/test/core/renametable_method_test.py
+++ src/test/core/renametable_method_test.py
@@ -1,9 +1,12 @@
 from pymtl import *
 from core.rtl.renametable import RenameTable
 from test.config import test_verilog
-from method_based import create_wrapper_class, rule, st, run_state_machine_as_test, create_test_state_machine, argument_strategy, reference_precondition, MethodOrder, StrategySpec, MethodStrategy
+from util.method_test import create_wrapper_class, rule, st, run_state_machine_as_test, create_test_state_machine, argument_strategy, reference_precondition, MethodOrder, ArgumentStrategy, MethodStrategy
 
 
+#-------------------------------------------------------------------------
+# RenameTableWrapper
+#-------------------------------------------------------------------------
 class RenameTableWrapper:
 
   def __init__( s, model ):
@@ -85,7 +88,10 @@ class RenameTableWrapper:
     s.model.restore_port.call.value = 0
 
 
-def run_test_method_base( rename_table ):
+#-------------------------------------------------------------------------
+# run_method_test
+#-------------------------------------------------------------------------
+def run_method_test( rename_table ):
   read_0 = rename_table.read_ports_0__call( 0 )
   read_1 = rename_table.read_ports_1__call( 1 )
   rename_table.write_ports_0__call( areg=1, preg=1 )
@@ -123,32 +129,44 @@ def run_test_method_base( rename_table ):
   rename_table.cycle()
 
 
-def test_method_base():
+#-------------------------------------------------------------------------
+# test_method
+#-------------------------------------------------------------------------
+def test_method():
   rename_table = RenameTableWrapper(
       RenameTable( 2, 4, 2, 1, 4, True, [ 0, 0 ] ) )
-  run_test_method_base( rename_table )
+  run_method_test( rename_table )
 
 
+#-------------------------------------------------------------------------
+# test_wrapper
+#-------------------------------------------------------------------------
 def test_wrapper():
   model = RenameTable( 2, 4, 2, 1, 4, True, [ 0, 0 ] )
   RenameTableWrapperClass = create_wrapper_class( model )
   rename_table = RenameTableWrapperClass( model )
 
-  run_test_method_base( rename_table )
+  run_method_test( rename_table )
 
 
+#-------------------------------------------------------------------------
+# RenameTableStrategy
+#-------------------------------------------------------------------------
 class RenameTableStrategy( MethodStrategy ):
 
   def __init__( s, naregs, npregs, nsnapshots ):
     s.Areg = st.integers( min_value=0, max_value=naregs - 1 )
     s.Preg = st.integers( min_value=0, max_value=npregs - 1 )
     s.Id = st.integers( min_value=0, max_value=nsnapshots - 1 )
-    s.read_ports = StrategySpec( areg=s.Areg )
-    s.write_ports = StrategySpec( areg=s.Areg, preg=s.Preg )
-    s.restore_port = StrategySpec( id=s.Id )
-    s.free_snapshot_port = StrategySpec( id=s.Id )
+    s.read_ports = ArgumentStrategy( areg=s.Areg )
+    s.write_ports = ArgumentStrategy( areg=s.Areg, preg=s.Preg )
+    s.restore_port = ArgumentStrategy( id=s.Id )
+    s.free_snapshot_port = ArgumentStrategy( id=s.Id )
 
 
+#-------------------------------------------------------------------------
+# RenameTableFL
+#-------------------------------------------------------------------------
 class RenameTableFL:
 
   def __init__( s, naregs, npregs, nread_ports, nwrite_ports, nsnapshots,
@@ -183,16 +201,10 @@ class RenameTableFL:
       return { "preg": s.reg_map_next[ areg ]}
     return { "preg": s.reg_map[ areg ]}
 
-  def read_ports_rdy( s ):
-    return True
-
   def write_ports_call( s, areg, preg ):
     assert areg < s.naregs and areg >= 0
     assert preg < s.npregs and preg >= 0
     s.reg_map[ areg ] = preg
-
-  def write_ports_rdy( s ):
-    return True
 
   def snapshot_port_call( s ):
     assert s.snapshot_port_rdy()
@@ -209,11 +221,7 @@ class RenameTableFL:
 
   def restore_port_call( s, id ):
     assert id >= 0 and id < s.nsnapshots
-    assert s.restore_port_rdy()
     s.reg_map_next = s.snap_shot[ id ][: ]
-
-  def restore_port_rdy( s ):
-    return True
 
   @reference_precondition(
       lambda machine, data: not data[ 'id' ] in machine.reference.snap_shot_free_list
@@ -234,6 +242,9 @@ class RenameTableFL:
       s.reg_map_next = []
 
 
+#-------------------------------------------------------------------------
+# test_fl
+#-------------------------------------------------------------------------
 def test_fl():
   rename_table = RenameTableFL( 2, 4, 2, 1, 1, True, [ 0, 0 ] )
 
@@ -244,6 +255,9 @@ def test_fl():
   assert read_1[ 'preg' ] == 0
 
 
+#-------------------------------------------------------------------------
+# test_state_machine
+#-------------------------------------------------------------------------
 def test_state_machine():
   RenameTableTest = create_test_state_machine(
       RenameTable( 2, 4, 2, 1, 1, True, [ 0, 0 ] ),
--- src/test/extra/ring_buffer_test.py
+++ src/test/extra/ring_buffer_test.py
@@ -1,9 +1,13 @@
 from pymtl import *
+from util.method_test import create_wrapper_class, rule, st, run_state_machine_as_test, create_test_state_machine, argument_strategy, reference_precondition, MethodOrder, ArgumentStrategy, MethodStrategy
 from util.test_utils import run_rdycall_test_vector_sim
 from util.rtl.ring_buffer import RingBuffer
 from test.config import test_verilog
 
 
+#-------------------------------------------------------------------------
+# test_basic_alloc
+#-------------------------------------------------------------------------
 def test_basic_alloc():
   run_rdycall_test_vector_sim(
       RingBuffer( NUM_ENTRIES=4, ENTRY_BITWIDTH=16 ),
@@ -28,6 +32,9 @@ def test_basic_alloc():
       test_verilog=test_verilog )
 
 
+#-------------------------------------------------------------------------
+# test_basic_update
+#-------------------------------------------------------------------------
 def test_basic_update():
   run_rdycall_test_vector_sim(
       RingBuffer( NUM_ENTRIES=4, ENTRY_BITWIDTH=16 ),
@@ -54,3 +61,108 @@ def test_basic_update():
       ],
       dump_vcd=None,
       test_verilog=test_verilog )
+
+
+#-------------------------------------------------------------------------
+# RenameTableStrategy
+#-------------------------------------------------------------------------
+
+
+class RingBufferStrategy( MethodStrategy ):
+
+  def __init__( s, NUM_ENTRIES, ENTRY_BITWIDTH ):
+    s.Index = st.integers( min_value=0, max_value=NUM_ENTRIES - 1 )
+    s.Value = st.integers( min_value=0, max_value=Bits( ENTRY_BITWIDTH )._max )
+    s.alloc_port = ArgumentStrategy( value=s.Value )
+    s.update_port = ArgumentStrategy( index=s.Index, value=s.Value )
+
+
+#-------------------------------------------------------------------------
+# RingBufferFL
+#-------------------------------------------------------------------------
+
+
+class RingBufferFL:
+
+  def __init__( s, NUM_ENTRIES, ENTRY_BITWIDTH ):
+    # We want to be a power of two so mod arithmetic is efficient
+    IDX_NBITS = clog2( NUM_ENTRIES )
+    assert 2**IDX_NBITS == NUM_ENTRIES
+    s.NUM_ENTRIES = NUM_ENTRIES
+    s.reset()
+    s.order = MethodOrder(
+        order=[ 'peek_port', 'alloc_port', 'update_port', 'remove_port' ] )
+    s.strategy = RingBufferStrategy( NUM_ENTRIES, ENTRY_BITWIDTH )
+    s.data = [ 0 ] * s.NUM_ENTRIES
+
+  def reset( s ):
+    s.head = 0
+    s.num = 0
+    s.next_slot = 0
+
+  def alloc_port_call( s, value ):
+    assert s.alloc_port_rdy()
+    index = s.next_slot
+    s.data[ index ] = value
+    s.num += 1
+    s.next_slot = ( s.next_slot + 1 ) % s.NUM_ENTRIES
+    return index
+
+  def alloc_port_rdy( s ):
+    return s.num < s.NUM_ENTRIES
+
+  def update_port_call( s, index, value ):
+    assert s.update_port_rdy()
+    assert index >= 0 and index < s.NUM_ENTRIES
+    s.data[ index ] = value
+
+  def update_port_rdy( s ):
+    return not s.empty()
+
+  def remove_port_call( s ):
+    assert s.remove_port_rdy()
+    s.head = ( s.head + 1 ) % s.NUM_ENTRIES
+    s.num -= 1
+
+  def remove_port_rdy( s ):
+    return not s.empty()
+
+  def peek_port_call( s ):
+    assert s.peek_port_rdy()
+    return s.data[ s.head ]
+
+  def peek_port_rdy( s ):
+    return not s.empty()
+
+  def empty( s ):
+    return s.num == 0
+
+
+#-------------------------------------------------------------------------
+# test_fl
+#-------------------------------------------------------------------------
+def test_fl():
+  ring_buffer = RingBufferFL( NUM_ENTRIES=4, ENTRY_BITWIDTH=16 )
+  assert ring_buffer.alloc_port_call( 0 ) == 0
+  assert ring_buffer.alloc_port_call( 1 ) == 1
+  assert ring_buffer.alloc_port_call( 2 ) == 2
+  assert ring_buffer.alloc_port_call( 3 ) == 3
+  assert ring_buffer.peek_port_call() == 0
+  ring_buffer.remove_port_call()
+  assert ring_buffer.peek_port_call() == 1
+  assert ring_buffer.alloc_port_call( 0 ) == 0
+  ring_buffer.update_port_call( 0, 1 )
+  ring_buffer.update_port_call( 1, 3 )
+  ring_buffer.update_port_call( 2, 5 )
+  ring_buffer.update_port_call( 3, 7 )
+  assert ring_buffer.peek_port_call() == 3
+
+
+#-------------------------------------------------------------------------
+# test_state_machine
+#-------------------------------------------------------------------------
+def test_state_machine():
+  RingBufferTest = create_test_state_machine(
+      RingBuffer( NUM_ENTRIES=4, ENTRY_BITWIDTH=16 ),
+      RingBufferFL( NUM_ENTRIES=4, ENTRY_BITWIDTH=16 ) )
+  run_state_machine_as_test( RingBufferTest )
