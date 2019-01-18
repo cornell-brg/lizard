0508882d5a45a104c3e8514e7c4a0fd1058814e1
Jacob Glueck <swimgiraffe435@gmail.com>
2019-01-18 09:25:39 -0500

    switch free list FL to bit-vector priority decoder

--- src/util/fl/freelist.py
+++ src/util/fl/freelist.py
@@ -7,57 +7,28 @@ class FreeListFL( Model ):
   def __init__( self, nslots ):
 
     self.nbits = clog2( nslots )
-    self.free_list = [ Bits( self.nbits ) for _ in range( nslots ) ]
-
-    ncount_bits = clog2( nslots + 1 )
-    self.size = Bits( ncount_bits )
-    self.head = Bits( self.nbits )
-    self.tail = Bits( self.nbits )
+    # 1 if free 0 otherwise
+    self.free_list = [ Bits( 1 ) for _ in range( nslots ) ]
 
   def xtick( self ):
     if self.reset:
       for x in range( len( self.free_list ) ):
-        self.free_list[ x ] = Bits( self.nbits, x )
-
-      self.head = Bits( self.nbits, 0 )
-      self.tail = Bits( self.nbits, 0 )
-      self.size = Bits( self.nbits, 0 )
-
-  def wrap_incr( self, x ):
-    if x == len( self.free_list ) - 1:
-      return Bits( x.nbits, 0 )
-    else:
-      return x + 1
-
-  def wrap_decr( self, x ):
-    if x == 0:
-      return Bits( x.nbits, len( self.free_list ) - 1 )
-    else:
-      return x - 1
+        self.free_list[ x ] = Bits( 1, 1 )
 
   # Returns either the tag or None if full
   def alloc( self ):
-    if self.size == len( self.free_list ):
-      return None
-    ret = self.free_list[ self.head ]
-    self.size += 1
-    self.head = self.wrap_incr( self.head )
-    return ret
+    for i in range( len( self.free_list ) ):
+      if self.free_list[ i ] == 1:
+        self.free_list[ i ] = 0
+        return Bits( self.nbits, i )
+
+    return None
 
   def free( self, tag ):
-    self.free_list[ self.tail ] = Bits( self.nbits, tag )
-    self.size -= 1
-    self.tail = self.wrap_incr( self.tail )
+    self.free_list[ tag ] = 1
 
   def line_trace( self ):
-    return "hd:{}tl:{}:sz:{}".format( self.head, self.tail, self.size )
+    return str( self )
 
   def __str__( self ):
-    if self.size == 0:
-      return '[ ]'
-    result = []
-    cur = self.head
-    while cur != self.wrap_decr( self.tail ):
-      result += [ self.free_list[ cur ] ]
-      cur = self.wrap_decr( cur )
-    return str( result )
+    return ''.join([ str( x ) for x in self.free_list ] )
