ce7db66c8b2f3e9ec8b144b4cc4a4184dbbaef6a
Jacob Glueck <swimgiraffe435@gmail.com>
2019-01-20 18:22:11 -0500

    demonstrate magic

--- src/test/extra/bug_test.py
+++ src/test/extra/bug_test.py
@@ -1,10 +1,10 @@
 from pymtl import *
 from util.test_utils import run_test_vector_sim
-from util.rtl.bug import Bug
+from util.rtl.bug import Bug, BugMagic
 
-def run_bug(test_verilog):
+def run_bug(model, test_verilog):
   run_test_vector_sim(
-      Bug(), [
+      model, [
           ( 'port.select port.out*' ),
           ( 0, 1 ),
           ( 1, 3 ),
@@ -12,10 +12,15 @@ def run_bug(test_verilog):
       dump_vcd=None,
       test_verilog=test_verilog)
 
-def test_basic_pymtl():
-  run_bug(False)
+def test_bug_pymtl():
+  run_bug(Bug(), False)
 
-def test_basic_verilog():
-  run_bug(True)
+def test_bug_verilog():
+  run_bug(Bug(), True)
 
+def test_bug_magic_pymtl():
+  run_bug(BugMagic(), False)
+
+def test_bug_magic_verilog():
+  run_bug(BugMagic(), True)
 
--- src/util/rtl/bug.py
+++ src/util/rtl/bug.py
@@ -41,3 +41,32 @@ class Bug( Model ):
   def line_trace( s ):
     return "{}".format(s.port.out)
 
+class BugMagic( Model ):
+
+  def __init__( s ):
+    s.interface = BugInterface()
+    s.port = s.interface.port.in_port()
+
+    s.outs = [Wire(2) for _ in range(2)]
+    s.magiczero = Wire(2)
+    s.connect(s.magiczero, 0)
+    
+    @s.combinational
+    def compute_1():
+      s.outs[s.magiczero].v = 1
+
+    @s.combinational
+    def compute_2():
+      if s.port.select:
+        s.outs[1].v = 3
+      else:
+        s.outs[1].v = s.outs[s.magiczero].v
+
+    @s.combinational
+    def compute_3():
+      s.port.out.v = s.outs[1]
+
+  def line_trace( s ):
+    return "{}".format(s.port.out)
+  
+
