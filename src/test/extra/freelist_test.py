9d9df7234c10f8eb2fb42c5406b832a28987d056
Jacob Glueck <swimgiraffe435@gmail.com>
2019-01-21 09:58:22 -0500

    test release in freelist

--- src/test/extra/freelist_test.py
+++ src/test/extra/freelist_test.py
@@ -63,3 +63,18 @@ def test_bypass():
       ],
       dump_vcd=None,
       test_verilog=test_verilog )
+
+
+def test_release():
+  run_test_vector_sim(
+      FreeList( 4, 1, 1, False, False ), [
+          ( 'alloc_ports[0].call alloc_ports[0].rdy* alloc_ports[0].index* alloc_ports[0].mask* free_ports[0].call free_ports[0].index release_port.call release_port.mask'
+          ),
+          ( 1, 1, 0, 0b0001, 0, 0, 0, 0b0000 ),
+          ( 1, 1, 1, 0b0010, 0, 0, 0, 0b0000 ),
+          ( 1, 1, 2, 0b0100, 0, 0, 1, 0b0011 ),
+          ( 1, 1, 0, 0b0001, 1, 1, 0, 0b0000 ),
+          ( 1, 1, 1, 0b0010, 1, 0, 0, 0b0000 ),
+      ],
+      dump_vcd=None,
+      test_verilog=test_verilog )
