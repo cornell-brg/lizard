8be77538750dcb681a0b021cc9a2635c37727baa
Aaron Wisner <daw268@cornell.edu>
2019-01-26 15:22:28 -0500

    Super clean generic issue queue

--- src/test/extra/issue_queue_test.py
+++ src/test/extra/issue_queue_test.py
@@ -1,44 +1,25 @@
 from pymtl import *
-from util.rtl.issue_queue import CompactingIssueQueue
-from core.rtl.issue import IssueSlot, MakeCompactingIssueQueue
+from util.rtl.issue_queue import CompactingIssueQueue, GenericIssueSlot, AbstractSlotType
 from test.config import test_verilog
 from util.test_utils import run_model_translation
 
 
+class TestSlotType(AbstractSlotType):
+  def __init__(s):
+    super(TestSlotType, s).__init__(5, 3)
+    s.priv = BitField(123)
+
+# Create a single instance of an issue slot
 def test_instatiate_slot():
-  run_model_translation( IssueSlot() )
+  run_model_translation( GenericIssueSlot(TestSlotType) )
 
 
 def test_instatiate_compacting_queue():
-  model = MakeCompactingIssueQueue( 10 )
+  make_slot_model = lambda : GenericIssueSlot(TestSlotType())
+  model =  CompactingIssueQueue(make_slot_model,
+                                TestSlotType(),
+                                TestSlotType().src0.nbits,
+                                TestSlotType().branch_mask.nbits,
+                                num_entries=10)
+
   run_model_translation( model )
-
-
-#-------------------------------------------------------------------------
-# test_basic_alloc
-#-------------------------------------------------------------------------
-# def test_instatiate():
-#   make_slot = lambda : SimpleIssueSlot(Bits(1), Bits(1), Bits(1), Bits(1))
-#   make_slot() # Test this
-#   CompactingIssueQueue(make_slot, Bits(1), Bits(1), Bits(1), Bits(1), 1, 1, 1)
-# run_rdycall_test_vector_sim(
-#     ReorderBuffer( NUM_ENTRIES=4, ENTRY_BITWIDTH=16 ),
-#     [
-#         ( 'alloc_port                    update_port              remove_port   peek_port       '
-#         ),
-#         ( 'arg(value), ret(index), call  arg(index, value), call  call          ret(value), call'
-#         ),
-#         (( 0, 0, 1 ), ( 0, 0, 0 ), ( 0 ),
-#          ( '?', 0 ) ),  # alloc index 0. value 0
-#         (( 1, 1, 1 ), ( 0, 0, 0 ), ( 0 ),
-#          ( 0, 1 ) ),  # alloc index 1, value 1
-#         (( 2, 2, 1 ), ( 0, 0, 0 ), ( 0 ),
-#          ( 0, 1 ) ),  # alloc index 2, value 1
-#         (( 3, 3, 1 ), ( 0, 0, 0 ), ( 0 ), ( 0, 1 ) ),
-#         (( 3, '?', 0 ), ( 0, 0, 0 ), ( 1 ), ( 0, 1 ) ),
-#         (( 3, '?', 0 ), ( 0, 0, 0 ), ( 1 ), ( 1, 1 ) ),
-#         (( 3, '?', 0 ), ( 0, 0, 0 ), ( 1 ), ( 2, 1 ) ),
-#         (( 3, '?', 0 ), ( 0, 0, 0 ), ( 1 ), ( 3, 1 ) ),
-#     ],
-#     dump_vcd=None,
-#     test_verilog=test_verilog )
--- src/util/rtl/issue_queue.py
+++ src/util/rtl/issue_queue.py
@@ -10,11 +10,11 @@ from util.rtl.interface import Interface
 from util.rtl.types import Array, canonicalize_type
 
 
+
 # Must support a ready(), valid(), input(), output(), notify(), kill()
 class IssueQueueSlotInterface( Interface ):
 
-  def __init__( s, SlotType, NotifyType, KillType, nports_notify=1 ):
-
+  def __init__( s, SlotType, NotifyType, BranchType, nports_notify=1 ):
     super( IssueQueueSlotInterface, s ).__init__([
         MethodSpec(
             'valid',
@@ -48,19 +48,18 @@ class IssueQueueSlotInterface( Interface ):
             rdy=False ),
         MethodSpec(
             'kill',
-            args={ 'value': KillType},
+            args={ 'value': BranchType},
             rets=None,
             call=True,
             rdy=False ),
-    ],
-                                                 ordering_chains=[
-                                                     [],
-                                                 ] )
+    ],ordering_chains=[
+      [],
+     ] )
 
 
 class IssueQueueInterface( Interface ):
 
-  def __init__( s, SlotType, NotifyType, KillType ):
+  def __init__( s, SlotType, NotifyType, BranchType ):
     super( IssueQueueInterface, s ).__init__([
         MethodSpec(
             'add', args={ 'value': SlotType}, rets=None, call=True, rdy=True ),
@@ -78,14 +77,81 @@ class IssueQueueInterface( Interface ):
             rdy=False ),
         MethodSpec(
             'kill',
-            args={ 'value': KillType},
+            args={ 'value': BranchType},
             rets=None,
             call=True,
             rdy=False ),
-    ],
-                                             ordering_chains=[
-                                                 [],
-                                             ] )
+    ], ordering_chains=[
+      [],
+     ] )
+
+
+
+class AbstractSlotType(BitStructDefinition):
+  def __init__(s, src_tag_nbits, branch_mask_nbits):
+    s.src0_valid = BitField(1)
+    s.src0_rdy = BitField(1)
+    s.src0 = BitField(src_tag_nbits)
+    s.src1_valid = BitField(1)
+    s.src1_rdy = BitField(1)
+    s.src1 = BitField(src_tag_nbits)
+    s.branch_mask = BitField(branch_mask_nbits)
+
+
+class GenericIssueSlot(Model):
+
+  def __init__(s, SlotType):
+    """ This model implements a generic issue slot, an issue queue has an instance
+      of this for each slot in the queue
+
+      SlotType: Should subclass AbstractSlotType and add any additional fields
+    """
+    s.iface = IssueQueueSlotInterface(SlotType(), SlotType().src0.nbits, SlotType().branch_mask.nbits)
+    s.iface.apply(s)
+
+    s.curr_ = Wire(SlotType())
+    s.valid_ = RegRst(Bits(1), reset_value=0)
+    s.srcs_ready_ = Wire(1)
+    s.kill_ = Wire(1)
+
+    # Does it match this cycle?
+    s.src0_match_ = Wire(1)
+    s.src1_match_ = Wire(1)
+
+    s.connect(s.curr_, s.output_value)
+
+    @s.combinational
+    def match_src():
+      s.src0_match_.v = s.curr_.src0_valid and s.notify_call and (not s.curr_.src0_rdy and s.curr_.src0 == s.notify_value)
+      s.src1_match_.v = s.curr_.src1_valid and s.notify_call and (not s.curr_.src1_rdy and s.curr_.src1 == s.notify_value)
+
+
+    @s.combinational
+    def handle_kill():
+      s.kill_.v = reduce_or(s.kill_value & s.curr_.branch_mask) and s.kill_call
+
+    @s.combinational
+    def handle_valid():
+      s.valid_.in_.v = not s.kill_ or (s.valid_.out and
+                                       not s.output_call) or s.input_call
+
+    @s.combinational
+    def handle_valrdy():
+      s.srcs_ready_.v = (((s.curr_.src0_rdy and s.curr_.src0_valid) or
+                          (not s.curr_.src0_valid) or s.src0_match_) and
+                         ((s.curr_.src1_rdy and s.curr_.src1_valid) or
+                          (not s.curr_.src1_valid) or s.src1_match_ ))
+
+      s.ready_ret.v = s.valid_.out and not s.kill_ and s.srcs_ready_
+      s.valid_ret.v = s.valid_.out
+
+    @s.tick_rtl
+    def update_slot():
+      if s.input_call:
+        s.curr_.n = s.input_value
+      else:
+        s.curr_.src0_rdy.n = s.curr_.src0_rdy or s.src0_match_
+        s.curr_.src1_rdy.n = s.curr_.src1_rdy or s.src1_match_
 
 
 class CompactingIssueQueue( Model ):
@@ -94,24 +160,33 @@ class CompactingIssueQueue( Model ):
                 create_slot,
                 SlotType,
                 NotifyType,
-                KillType,
+                BranchType,
                 num_entries,
                 alloc_nports=1,
-                issue_nports=1 ):
+                issue_nports=1):
     """ This model implements a generic issue queue
 
-      create_slot: A function that instatiates a IssueSlot model
-                    that conforms to the IssueSlot interface
+      create_slot: A function that instatiates a model that conforms
+                    to the IssueSlot interface. In most cases, GenericIssueSlot
+                    can be used instead of having to implement your own model
 
       input_type: the data type that wil be passed via the input() method
                   to the issue slot
+
+      SlotType: A Bits() or BitStruct() that contains the data stored in each issue slot (IS)
+
+      NotifyType: A Bits() or BitStruct() that will be passed in as an arg to the notify method
+
+      BranchType: A Bits() or BitStruct() that will be broadcasted to each IS when a branch/kill event happens
+
+      num_entries: The number of slots in the IQ
     """
     assert alloc_nports == 1  # Only 1 port supported for now
     assert issue_nports == 1
 
     s.idx_nbits = clog2( num_entries )
-    s.KillDtype = canonicalize_type( KillType )
-    s.interface = IssueQueueInterface( SlotType, NotifyType, KillType )
+    s.KillDtype = canonicalize_type( BranchType )
+    s.interface = IssueQueueInterface( SlotType, NotifyType, BranchType )
     s.interface.apply( s )
 
     # Create all the slots in our issue queue
