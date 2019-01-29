916473c011de5bc2326328d5084702476dbd5a7b
Aaron Wisner <daw268@cornell.edu>
2019-01-29 16:55:33 -0500

    Construct DataFlow with correct nargs

--- src/core/rtl/proc.py
+++ src/core/rtl/proc.py
@@ -3,10 +3,11 @@ from core.rtl.controlflow import ControlFlowManager, ControlFlowManagerInterface
 from core.rtl.fetch import Fetch
 from core.rtl.dataflow import DataFlowManager
 
+from config.general import *
 
 class Proc(Model):
   def __init__(s):
     s.cflow_ = ControlFlowManager()
-    s.dflow_ = DataFlowManager(2, 1)
+    s.dflow_ = DataFlowManager(AREG_COUNT, PREG_COUNT, MAX_SPEC_DEPTH, 2, 1)
 
     s.fetch_ = Fetch()
