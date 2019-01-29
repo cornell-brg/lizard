9ed6ad65039e3c3a856d8fca39c9bdc02b1cb063
Aaron Wisner <daw268@cornell.edu>
2019-01-29 16:28:49 -0500

    Added initial proc rtl files

---
+++ src/core/rtl/proc.py
@@ -0,0 +1,8 @@
+from pymtl import *
+from core.rtl.controlflow import ControlFlowManager, ControlFlowManagerInterface
+from core.rtl.fetch import Fetch
+
+
+class Proc(Model):
+  def __init__(s):
+    pass
