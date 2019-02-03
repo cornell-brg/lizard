44ba37675de59d6c390fc18b84117a40a5401374
Jacob Glueck <swimgiraffe435@gmail.com>
2019-02-03 11:03:48 -0500

    allow type wrapping

--- src/model/wrapper.py
+++ src/model/wrapper.py
@@ -1,3 +1,4 @@
+from inspect import isclass
 from pymtl import *
 from model.flmodel import FLModel
 from model.clmodel import CLModel
@@ -7,34 +8,67 @@ from model.fladapter import FLAdapter
 from model.cladapter import CLAdapter
 
 
-def wrap_to_rtl(model):
-  if isinstance(model, Model):
-    return model
-  elif isinstance(model, CLModel):
-    return CLAdapter(model)
-  elif isinstance(model, FLModel):
-    return CLAdapter(FLAdapter(model))
+class Wrapper(object):
+
+  def __init__(s, wrapped, wrapping):
+    s.wrapped = wrapped
+    s.wrapping = wrapping
+
+  def __call__(s, *args, **kwargs):
+    if isclass(s.wrapped) or isinstance(s.wrapped, Wrapper):
+      inside = s.wrapped(*args, **kwargs)
+    else:
+      inside = s.wrapped
+    return s.wrapping(inside)
+
+
+def class_type(obj):
+  if isclass(obj):
+    return obj
   else:
-    raise ValueError('Unknown model type: {}'.format(type(model)))
+    return type(obj)
 
 
-def wrap_to_cl(model):
-  if isinstance(model, Model):
-    return RTLWrapper(model)
-  elif isinstance(model, CLModel):
-    return model
-  elif isinstance(model, FLModel):
-    return FLAdapter(model)
-  else:
-    raise ValueError('Unknown model type: {}'.format(type(model)))
+def wrap(model, wrap_spec):
+  model_type = class_type(model)
+  result = model
+  for layer in wrap_spec:
+    result = Wrapper(result, layer)
+
+  # if the input was not a class, evaluate it
+  # make sure we actually wrapped it as well
+  if not isclass(model) and isinstance(result, Wrapper):
+    result = result()
+
+  return result
 
 
-def wrap_to_fl(model):
-  if isinstance(model, Model):
-    return CLWrapper(RTLWrapper(model))
-  elif isinstance(model, CLModel):
-    return CLWrapper(model)
-  elif isinstance(model, FLModel):
-    return model
-  else:
-    raise ValueError('Unknown model type: {}'.format(type(model)))
+def gen_wrap(wrap_spec):
+
+  def do_wrap(model):
+    model_type = class_type(model)
+    for target_type, spec in wrap_spec.iteritems():
+      if issubclass(model_type, target_type):
+        return wrap(model, spec)
+    raise ValueError('Unable to wrap type: {}'.format(model_type))
+
+  return do_wrap
+
+
+wrap_to_rtl = gen_wrap({
+    Model: [],
+    CLModel: [CLAdapter],
+    FLModel: [FLAdapter, CLAdapter],
+})
+
+wrap_to_cl = gen_wrap({
+    Model: [RTLWrapper],
+    CLModel: [],
+    FLModel: [FLAdapter],
+})
+
+wrap_to_fl = gen_wrap({
+    Model: [RTLWrapper, CLWrapper],
+    CLModel: [CLWrapper],
+    FLModel: [],
+})
