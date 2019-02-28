f98dcd7e9debd3399aaa49609176b39af1fda7f5
Aaron Wisner <daw268@cornell.edu>
2019-02-27 20:54:03 -0500

    Add asm test to proc

--- src/test/core/proc_rtl/proc_test.py
+++ src/test/core/proc_rtl/proc_test.py
@@ -9,7 +9,7 @@ from util.rtl.interface import Interface, UseInterface, IncludeAll
 from mem.rtl.memory_bus import MemoryBusInterface, MemMsgType
 from mem.fl.test_memory_bus import TestMemoryBusFL
 from core.rtl.proc import ProcInterface, Proc
-
+from util.arch.rv64g import isa, assembler
 
 class ProcTestHarness(Model):
 
@@ -23,9 +23,6 @@ class ProcTestHarness(Model):
     s.connect_m(s.mb.recv, s.dut.mb_recv)
     s.connect_m(s.mb.send, s.dut.mb_send)
 
-
-# def test_translate_fetch():
-#   run_model_translation(Fetch(64, 32, 2, ))
 
 
 def test_basic():
@@ -45,4 +42,24 @@ def test_basic():
 
   dut.reset()
   for i in range(2 * len(data)):
+    dut.cycle()
+
+
+def test_asm():
+  asm = """
+  addi x1, x2, 0
+  add x3, x4, x5
+  sub x6, x7, x8
+  """
+  mem_image = assembler.assemble(asm).get_section(".text").data
+  initial_mem = {}
+  # There has to be a better way to do this
+  for i,b in enumerate(mem_image):
+    initial_mem[i + 0x200] = b
+
+  pth = ProcTestHarness(initial_mem)
+  dut = wrap_to_cl(pth)
+
+  dut.reset()
+  for i in range(20):
     dut.cycle()
