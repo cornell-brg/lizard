2b7f4bfc9cf6539cd73f33c7d17a7e55ef9ac25e
Jacob Glueck <swimgiraffe435@gmail.com>
2019-03-01 07:43:31 -0500

    move wrapper __del__ patch to pymtl

--- src/model/translate.py
+++ src/model/translate.py
@@ -2,35 +2,16 @@ from pymtl import *
 
 
 def translate_class(model):
-  result_model = TranslationTool(model, lint=True)
-  # Generated moodel has this code:
-  # def __del__( s ):
-  #   s._ffi.destroy_model( s._m )
-  # This means it can't be destroyed before it's been elaborated
-  # or a strange message is printed:
-  # Exception AttributeError: "'Proc_0x61a5b3756704d9a6' object has no attribute
-  # '_m'" in <bound method Proc_0x61a5b3756704d9a6.__del__ of
-  # <Proc_0x61a5b3756704d9a6_v.Proc_0x61a5b3756704d9a6 object at
-  # 0x7fd465eb5350>> ignored
-  # So:
-  # 1) Elaborate result_model to avoid that
-  # 2) Monkey patch the class to fix this behavior
-  result_class = result_model.__class__
-
-  result_model.elaborate_logic()
-  result_class._old_del = result_class.__del__
-
-  def sane_del(s):
-    if hasattr(s, '_m'):
-      s._old_del()
-
-  result_class.__del__ = sane_del
+  result_class = TranslationTool(model, lint=True).__class__
 
   # Monkey patch init such that each instantiation of the translated
   # model has an interface inside
+  # Also copy over the VCD file name
   def embed_init(s, *args, **kwargs):
     s._old_init(*args, **kwargs)
     model.interface.embed(s, model._requirements)
+    if hasattr(model, 'vcd_file'):
+      s.vcd_file = model.vcd_file
 
   result_class._old_init = result_class.__init__
   result_class.__init__ = embed_init
