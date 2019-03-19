227a504830eb09b0095be5b3b3c970c152a1429e
Jacob Glueck <swimgiraffe435@gmail.com>
2019-03-19 14:48:49 -0400

    add 1 not taken branch test

--- src/test/core/inst/branch/inst_beq.py
+++ src/test/core/inst/branch/inst_beq.py
@@ -50,6 +50,48 @@ def gen_basic_test():
 
   """
 
+#-------------------------------------------------------------------------
+# gen_basic_test
+#-------------------------------------------------------------------------
+def gen_basic_not_taken_test():
+  return """
+
+    # Use x3 to track the control flow pattern
+    addi  x3, x0, 0
+
+    csrr  x1, mngr2proc < 2
+    csrr  x2, mngr2proc < 3
+
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+    # This branch should be taken
+    beq   x1, x2, label_a
+    addi  x3, x3, 0b01
+
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+  label_a:
+    addi  x3, x3, 0b10
+
+    # Only the second bit should be set if branch was taken
+    csrw proc2mngr, x3 > 0b11
+
+  """
+
 
 # ''' LAB TASK ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # Define additional directed and random test cases.
