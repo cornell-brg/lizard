fb21f9fd1ab36be6d437604d81c9215048540d53
Jacob Glueck <swimgiraffe435@gmail.com>
2019-03-19 14:52:13 -0400

    add 3 branches

--- src/test/core/inst/branch/inst_beq.py
+++ src/test/core/inst/branch/inst_beq.py
@@ -92,6 +92,112 @@ def gen_basic_not_taken_test():
 
   """
 
+#-------------------------------------------------------------------------
+# gen_basic_test
+#-------------------------------------------------------------------------
+def gen_basic_2_test():
+  return """
+
+    # Use x3 to track the control flow pattern
+    addi  x3, x0, 0
+
+    csrr  x1, mngr2proc < 2
+    csrr  x2, mngr2proc < 2
+
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+    # This branch should be taken
+    beq   x1, x2, label_a
+    addi  x3, x3, 0b01
+
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+  label_a:
+    addi  x3, x3, 0b10
+
+    # Only the second bit should be set if branch was taken
+    csrw proc2mngr, x3 > 0b10
+    
+    addi  x3, x0, 0
+
+    csrr  x1, mngr2proc < 2
+    csrr  x2, mngr2proc < 2
+
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+    # This branch should be taken
+    beq   x1, x2, label_b
+    addi  x3, x3, 0b01
+
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+  label_b:
+    addi  x3, x3, 0b10
+
+    # Only the second bit should be set if branch was taken
+    csrw proc2mngr, x3 > 0b10
+
+    addi  x3, x0, 0
+
+    csrr  x1, mngr2proc < 2
+    csrr  x2, mngr2proc < 2
+
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+    # This branch should be taken
+    beq   x1, x2, label_c
+    addi  x3, x3, 0b01
+
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+  label_c:
+    addi  x3, x3, 0b10
+
+    # Only the second bit should be set if branch was taken
+    csrw proc2mngr, x3 > 0b10
+  """
 
 # ''' LAB TASK ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # Define additional directed and random test cases.
