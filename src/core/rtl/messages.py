342c69c0c41dc550d8cab41129461c41144452cc
Jacob Glueck <swimgiraffe435@gmail.com>
2019-03-19 16:43:18 -0400

    add serialize to decode payload

--- src/core/rtl/messages.py
+++ src/core/rtl/messages.py
@@ -206,6 +206,7 @@ FetchMsg = FrontendMsg(FetchPayload())
 def DecodePayload():
   return [
       Field('speculative', 1),
+      Field('serialize', 1),
       Field('pc_succ', XLEN),
       ValidValuePair('rs1', AREG_IDX_NBITS),
       ValidValuePair('rs2', AREG_IDX_NBITS),
