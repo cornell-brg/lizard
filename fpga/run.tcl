d74ada271a0f2bae4d03b90defbacdeb1b450e67
Jacob Glueck <swimgiraffe435@gmail.com>
2019-03-22 20:25:25 -0400

    switch to ultrascale + and run implementation

--- fpga/run.tcl
+++ fpga/run.tcl
@@ -1,12 +1,36 @@
 set prj fpga-rv64gc
 set top proc
 
-create_project ${prj} ${prj} -part xc7z020clg484-1 -force
+# See: https://www.xilinx.com/support/documentation/sw_manuals/xilinx2013_4/ug894-vivado-tcl-scripting.pdf
+
+# ZedBoard
+# create_project ${prj} ${prj} -part xc7z020clg484-1 -force
+# Zynq UltraScale+
+create_project ${prj} ${prj} -part xczu7ev-ffvf1517-1LV-i -force
 file mkdir ${prj}/${prj}.srcs/
 file mkdir ${prj}/${prj}.runs/synth/
 file copy ${top}.sv ${prj}/${prj}.srcs/
 add_files ${prj}/${prj}.srcs/
 update_compile_order -fileset sources_1
-export_ip_user_files -of_objects  [get_files ${prj}/${prj}.srcs/] -no_script -reset -force -quiet
+
+# Synthesis
 synth_design -top ${top}
 report_utilization -file ${prj}/${prj}.runs/synth/${top}_utilization_synth.rpt
+
+# Optimize
+opt_design
+
+# Place
+place_design
+
+# Route
+route_design
+
+report_route_status -file ${prj}/${prj}.runs/post_route_status.rpt
+report_timing_summary -file ${prj}/${prj}.runs/post_route_timing_summary.rpt
+report_power -file ${prj}/${prj}.runs/post_route_power.rpt
+report_drc -file ${prj}/${prj}.runs/post_imp_drc.rpt
+
+# Bitstream
+
+write_bitstream -force ${prj}/${prj}.runs/${top}.bit
