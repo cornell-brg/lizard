46008cd9e5985c8a34cb19a2fc7bce081d5db7b4
Jacob Glueck <swimgiraffe435@gmail.com>
2019-04-11 22:03:05 -0400

    write lookup registerfile

---
+++ src/util/fl/lookup_registerfile.py
@@ -0,0 +1,41 @@
+from pymtl import *
+
+from model.hardware_model import HardwareModel, Result
+from model.flmodel import FLModel
+
+
+class LookupRegisterFileFL(FLModel):
+
+  @HardwareModel.validate
+  def __init__(s, interface, key_groups, reset_values=None):
+    super(LookupRegisterFileFL, s).__init__(interface)
+
+    size = len(key_groups)
+    if reset_values is None:
+      reset_values = [0] * size
+    s.state(values={i: int(reset_values[i]) for i in range(size)})
+
+    mapping = {}
+    for i, group in enumerate(key_groups):
+      if not isinstance(group, tuple):
+        group = (group,)
+      for key in group:
+        assert key not in mapping
+        mapping[key] = i
+
+    @s.model_method
+    def read(key):
+      key = int(key)
+      if key in mapping:
+        return Result(value=s.values[mapping[key]], valid=1)
+      else:
+        return Result(value=0, valid=0)
+
+    @s.model_method
+    def write(key, value):
+      key = int(key)
+      if key in mapping:
+        s.values[mapping[key]] = value
+        return 1
+      else:
+        return 0
