e8a0370798d8839252d7be7cd2c2775bbd0b503a
Aaron Wisner <daw268@cornell.edu>
2019-04-16 18:08:31 -0400

    Fix all divides and rems

--- src/core/rtl/backend/multiply.py
+++ src/core/rtl/backend/multiply.py
@@ -40,7 +40,7 @@ class MultInternal(Model):
         for m in PipelineStageInterface(MultIn(), None).methods.values()
     ])
     # s.multiplier = MulPipelined(MulPipelinedInterface(XLEN, keep_upper=True),
-    #                                           nstages=num_stages, use_mul=True)
+    #                                            nstages=num_stages, use_mul=True)
     # # TODO: AARON
     # # You have:
     # # in_peek (rdy, msg), where msg is MultIn
@@ -107,7 +107,7 @@ class MultOutputPipelineAdapter(Model):
       s.out_temp.v = s.fuse_kill_data
       # TODO AARON YOU CAN READ THE RESULT FROM fuse_kill_data.result
       # AND USE IT TO POST-PROCESS THE INTERNAL OUT
-      s.out_temp.result.v = s.fuse_internal_out
+      s.out_temp.result.v = s.fuse_internal_out[:64]
 
     s.connect(s.fuse_out, s.out_temp)
 
