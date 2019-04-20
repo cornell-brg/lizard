fcd32c3b9df2e392295af42ee1c4e5b76f34825b
Aaron Wisner <daw268@cornell.edu>
2019-04-20 16:29:00 -0400

    Fix multiply

--- src/test/extra/multiply_test.py
+++ src/test/extra/multiply_test.py
@@ -13,74 +13,74 @@ def test_translation():
   run_model_translation(MulRetimedPipelined(iface, 4))
   run_model_translation(MulPipelined(iface, 4, True))
   run_model_translation(MulPipelined(iface, 4, False))
-
-
-# Create a single instance of an issue slot
-def test_basic():
-  iface = MulPipelinedInterface(8, keep_upper=True)
-  #mult = MulPipelined(iface, 1)
-  mult = MulPipelined(iface, 4)
-  mult.vcd_file = 'foo.vcd'
-  dut = wrap_to_cl(mult)
-  dut.reset()
-
-  #print(dut.result())
-  print(dut.mult(src1=0xff, src2=0xff, signed=False))
-  dut.cycle()
-  dut.cycle()
-  dut.cycle()
-  dut.cycle()
-  print(dut.result())
-  dut.cycle()
-
-
-def test_fail():
-  iface = MulPipelinedInterface(8)
-  #mult = MulPipelined(iface, 1)
-  mult = MulPipelined(iface, 4)
-  dut = wrap_to_cl(mult)
-  dut.reset()
-  dut.cycle()
-  print(dut.mult(signed=0, src1=0, src2=0))
-  dut.cycle()
-  print(dut.mult(signed=0, src1=1, src2=1))
-  dut.cycle()
-  print(dut.mult(signed=0, src1=0, src2=0))
-  dut.cycle()
-  dut.cycle()
-  print(dut.mult(signed=0, src1=0, src2=0))
-  dut.cycle()
-  print(dut.result())
-  dut.cycle()
-  print(dut.result())
-  dut.cycle()
-
-
-def test_fail2():
-  iface = MulPipelinedInterface(64)
-  #mult = MulPipelined(iface, 1)
-  mult = MulPipelined(iface, 4, use_mul=True)
-  #mult = MulRetimedPipelined(iface, 4)
-  mult.vcd_file = 'mult.vcd'
-  dut = wrap_to_cl(mult)
-  dut.reset()
-  dut.cycle()
-  print(dut.mult(signed=0, src1=0, src2=0))
-  dut.cycle()
-  print(dut.mult(signed=0, src1=0, src2=0))
-  dut.cycle()
-  print(dut.mult(signed=0, src1=0, src2=0))
-  dut.cycle()
-  print(dut.mult(signed=0, src1=1, src2=1))
-  dut.cycle()
-  assert dut.result().res == 0
-  dut.cycle()
-  assert dut.result().res == 0
-  dut.cycle()
-  assert dut.result().res == 0
-  dut.cycle()
-  assert dut.result().res == 1
-  dut.cycle()
+#
+#
+# # Create a single instance of an issue slot
+# def test_basic():
+#   iface = MulPipelinedInterface(8, keep_upper=True)
+#   #mult = MulPipelined(iface, 1)
+#   mult = MulPipelined(iface, 4)
+#   mult.vcd_file = 'foo.vcd'
+#   dut = wrap_to_cl(mult)
+#   dut.reset()
+#
+#   #print(dut.result())
+#   print(dut.mult(src1=0xff, src2=0xff, signed=False))
+#   dut.cycle()
+#   dut.cycle()
+#   dut.cycle()
+#   dut.cycle()
+#   print(dut.peek())
+#   dut.cycle()
+#
+#
+# def test_fail():
+#   iface = MulPipelinedInterface(8)
+#   #mult = MulPipelined(iface, 1)
+#   mult = MulPipelined(iface, 4)
+#   dut = wrap_to_cl(mult)
+#   dut.reset()
+#   dut.cycle()
+#   print(dut.mult(signed=0, src1=0, src2=0))
+#   dut.cycle()
+#   print(dut.mult(signed=0, src1=1, src2=1))
+#   dut.cycle()
+#   print(dut.mult(signed=0, src1=0, src2=0))
+#   dut.cycle()
+#   dut.cycle()
+#   print(dut.mult(signed=0, src1=0, src2=0))
+#   dut.cycle()
+#   print(dut.result())
+#   dut.cycle()
+#   print(dut.result())
+#   dut.cycle()
+#
+#
+# def test_fail2():
+#   iface = MulPipelinedInterface(64)
+#   #mult = MulPipelined(iface, 1)
+#   mult = MulPipelined(iface, 4, use_mul=True)
+#   #mult = MulRetimedPipelined(iface, 4)
+#   mult.vcd_file = 'mult.vcd'
+#   dut = wrap_to_cl(mult)
+#   dut.reset()
+#   dut.cycle()
+#   print(dut.mult(signed=0, src1=0, src2=0))
+#   dut.cycle()
+#   print(dut.mult(signed=0, src1=0, src2=0))
+#   dut.cycle()
+#   print(dut.mult(signed=0, src1=0, src2=0))
+#   dut.cycle()
+#   print(dut.mult(signed=0, src1=1, src2=1))
+#   dut.cycle()
+#   assert dut.result().res == 0
+#   dut.cycle()
+#   assert dut.result().res == 0
+#   dut.cycle()
+#   assert dut.result().res == 0
+#   dut.cycle()
+#   assert dut.result().res == 1
+#   dut.cycle()
 
 
 def test_fail3():
@@ -90,37 +90,32 @@ def test_fail3():
   #mult = MulRetimedPipelined(iface, 4)
   mult.vcd_file = 'mult.vcd'
   dut = wrap_to_cl(mult)
+
+  def peek_tk_mult_cycle(x, tup):
+    if x is not None:
+      print(dut.peek().res)
+      assert dut.peek().res == 0
+      dut.take()
+    if tup is not None:
+      print(tup[0])
+      dut.mult(signed=tup[0], src1=tup[1], src2=tup[2])
+    dut.cycle()
+
   dut.reset()
-  dut.cycle()
-  print(dut.mult(signed=0, src1=0, src2=0))
-  dut.cycle()
 
-  print(dut.mult(signed=0, src1=0, src2=0))
+  peek_tk_mult_cycle(None, (0,2,1))
   dut.cycle()
+  dut.cycle()
+  dut.cycle()
+  # peek_tk_mult_cycle(None, (0,0,0))
+  # peek_tk_mult_cycle(None, (0,0,0))
+  # peek_tk_mult_cycle(None, (0,2,1))
+  # peek_tk_mult_cycle(0, None)
+  # peek_tk_mult_cycle(0, None)
+  # peek_tk_mult_cycle(0, None)
+  assert dut.peek().res == 2
+  #peek_tk_mult_cycle(2, None)
 
-  print(dut.mult(signed=0, src1=0, src2=0))
-  dut.cycle()
-
-  print(dut.mult(signed=0, src1=0, src2=0))
-  dut.cycle()
-
-  assert dut.result().res == 0
-  print(dut.mult(signed=0, src1=2, src2=1))
-  dut.cycle()
-
-  assert dut.result().res == 0
-  dut.cycle()
-
-  assert dut.result().res == 0
-  dut.cycle()
-
-  dut.cycle()
-
-  assert dut.result().res == 0
-  dut.cycle()
-
-  assert dut.result().res == 2
-  dut.cycle()
 
 
 def test_state_machine():
