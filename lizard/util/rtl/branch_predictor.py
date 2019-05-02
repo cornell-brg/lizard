2f729ac64c2e98e26c6b63aa8b5a84534a3dd011
Jacob Glueck <swimgiraffe435@gmail.com>
2019-05-01 20:12:25 -0400

    format

--- lizard/util/rtl/branch_predictor.py
+++ lizard/util/rtl/branch_predictor.py
@@ -11,41 +11,43 @@ class BranchPredictorInterface(Interface):
   def __init__(s, pc_nbits):
     s.pc_nbits = pc_nbits
 
-    super(BranchPredictorInterface, s).__init__(
-        [
-            MethodSpec(
-                'predict',
-                args={
-                    'pc': s.pc_nbits,
-                },
-                call=True,
-                rdy=False,
-            ),
-            MethodSpec(
-                'prediction',
-                rets={
-                    'taken': Bits(1),
-                },
-                call=False,
-                rdy=True,
-            ),
-            MethodSpec(
-                'update',
-                args={
-                    'pc': s.pc_nbits,
-                    'taken' : Bits(1),
-                },
-                call=True,
-                rdy=False,
-            ),
-        ],
-    )
-
+    super(BranchPredictorInterface, s).__init__([
+        MethodSpec(
+            'predict',
+            args={
+                'pc': s.pc_nbits,
+            },
+            call=True,
+            rdy=False,
+        ),
+        MethodSpec(
+            'prediction',
+            rets={
+                'taken': Bits(1),
+            },
+            call=False,
+            rdy=True,
+        ),
+        MethodSpec(
+            'update',
+            args={
+                'pc': s.pc_nbits,
+                'taken': Bits(1),
+            },
+            call=True,
+            rdy=False,
+        ),
+    ],)
 
 
 class GlobalBranchPredictor(Model):
 
-  def __init__(s, interface, hist_idx_nbits, hist_nbits, pht_nbits, hasher='concat'):
+  def __init__(s,
+               interface,
+               hist_idx_nbits,
+               hist_nbits,
+               pht_nbits,
+               hasher='concat'):
     UseInterface(s, interface)
     assert hasher in ['concat', 'xor']
 
@@ -57,13 +59,15 @@ class GlobalBranchPredictor(Model):
     assert pc_hash_nbits > 0
 
     # This stores the global history
-    hist_iface = SynchronousRAMInterface(hist_nbits, 2**hist_idx_nbits, 2, 1, False)
+    hist_iface = SynchronousRAMInterface(hist_nbits, 2**hist_idx_nbits, 2, 1,
+                                         False)
     s.history = SynchronousRAM(hist_iface)
 
     pht_iface = SynchronousRAMInterface(2, 2**pht_nbits, 1, 1, False)
     s.pht = SynchronousRAM(hist_iface)
 
-    s.reg_pc_predict = Register(RegisterInterface(Bits(s.interface.pc_nbits), enable=True))
+    s.reg_pc_predict = Register(
+        RegisterInterface(Bits(s.interface.pc_nbits), enable=True))
     s.connect(s.reg_pc_predict.write_call, s.predict_call)
     s.connect(s.reg_pc_predict.write_data, s.predict_pc)
 
@@ -88,6 +92,7 @@ class GlobalBranchPredictor(Model):
         s.pht_pc_idx.v = s.history.read_data[0][0:hist_nbits]
 
     if hist_idx_nbits > 0:
+
       @s.combinational
       def pc_index():
         s.history.read_next[0].v = 0
@@ -97,14 +102,15 @@ class GlobalBranchPredictor(Model):
       s.connect(s.history.read_next[0], 0)
 
     if hasher == 'concat':
+
       @s.combinational
       def pc_idx():
         s.pht_idx.v = (s.pht_pc_idx << hist_nbits) | s.pht_hist_idx
     else:
+
       @s.combinational
       def pc_idx():
         s.pht_idx.v = s.pht_pc_idx ^ s.pht_hist_idx
-
 
   def line_trace(s):
     return ":".join(["{}".format(x) for x in s.regs])
