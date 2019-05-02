e9608a32957315d0bc28e4330607b3a075517d33
Jacob Glueck <swimgiraffe435@gmail.com>
2019-05-01 22:07:10 -0400

    update poster test

--- tests/core/inst/extra/inst_extra.py
+++ tests/core/inst/extra/inst_extra.py
@@ -31,20 +31,14 @@ def gen_write_x0_test():
 
 def poster_demo_test():
   return """
-    addi   x2, x0, 0
-    addi   x3, x0, 20
-    addi   x4, x0, 40
-    addi   x5, x0, 5
+    addi   x1, x0, 0
+    addi   x2, x0, 20
   loop:
-    lw    x6, 0(x2)
-    lw    x7, 0(x3)
-    add   x8, x6, x7
-    sw    x8, 0(x4)
-    addi  x2, x2, 4
-    addi  x3, x3, 4
-    addi  x4, x4, 4
-    addi  x5, x5, -1
-    bne   x5, x0, loop
+    lw    x3, 0(x1)
+    mul   x3, x3, x3
+    sw    x3, 0(x1)
+    addi  x1, x1, 4
+    bne   x1, x2, loop
     nop
     nop
     nop
